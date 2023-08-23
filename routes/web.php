064a367a77434ca30ad253a7e67fe2a2d022adff
Juan Francisco Markan <jfmarkan@gmail.com>
2023-08-23 16:10:16 +0200

    updated routes in web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController as GuestPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/',[GuestPageController::class, 'landing'])->name('guest.welcome');
 
 Auth::routes();
 
